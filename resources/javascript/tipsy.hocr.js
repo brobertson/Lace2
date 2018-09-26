6fda8ab1c07482605900a22925e3ae2a8bba0331
Bruce Robertson <brobertson@mta.ca>
2018-09-26 10:46:26 -0300

    javascript for cropped images

--- resources/javascript/tipsy.hocr.js
+++ resources/javascript/tipsy.hocr.js
@@ -86,7 +86,7 @@ function add_index_after(element, e,uniq) {
 function generate_image_tag_call(book_name, page_file, bbox) {
     //book_name = "490021999brucerob"
     //page_file = "490021999brucerob_0100.jpg"
-        var request = "<img src=\"" + "http://heml.mta.ca/lace" + "/image_test?book=" + encodeURIComponent(book_name) + "&amp;file=" + encodeURIComponent(page_file) + "&amp;bbox=" + encodeURIComponent(bbox) + "\" alt='a word image'/>"
+        var request = "<img src=\"" + "getCroppedImage.xq?book=" + encodeURIComponent(book_name) + "&amp;file=" + encodeURIComponent(page_file) + "&amp;bbox=" + encodeURIComponent(bbox) + "\" alt='a word image'/>"
         //console.log(request);
 	return request
 }
